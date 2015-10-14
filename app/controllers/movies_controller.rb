435dbd01c24a1a180fdc119e6d7f28ac8db7f151
Jennifer Bell & Lisa Folliard <student@epicodus.com>
2015-10-14 11:00:45 -0700

    add movie update functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,19 @@ class MoviesController <ApplicationController
     end
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path(@movie)
+    else
+      render :edit
+    end
+  end
+
   private
   def movie_params
     params.require(:movie).permit(:title, :director, :synopsis)
