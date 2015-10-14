051b0626d5559949ec6dbf1965d201c2d54692e3
Jennifer Bell & Lisa Folliard <student@epicodus.com>
2015-10-14 10:50:43 -0700

    Add ability to create a new movie and display it on the individual movie page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,21 @@ class MoviesController <ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-   def new
+  def new
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :director, :synopsis)
+  end
 end
