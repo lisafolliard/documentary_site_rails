293f2c21ce56d3ec635df97cf31e07cd7c3e9ca5
Jennifer Bell & Lisa Folliard <student@epicodus.com>
2015-10-14 14:47:50 -0700

    add ability to update individual review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,6 +15,21 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to movie_path(@review.movie)
+    else
+      render :edit
+    end
+  end
+
   private
   def review_params
     params.require(:review).permit(:name, :comment, :rating)
