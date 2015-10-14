ce3937a41d9a9248e5ad23be2562a1508c276373
Jennifer Bell & Lisa Folliard <student@epicodus.com>
2015-10-14 11:47:03 -0700

    Add files for integration testing to pass first spec of adding a movie

---
+++ spec/features/add_movie_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "the add a movie process" do
+  it "adds a new movie" do
+    visit movies_path
+    click_on "Add a new Movie"
+    fill_in 'Title', :with => 'Test Title'
+    fill_in 'Director', :with => 'That French Guy'
+    fill_in 'Synopsis', :with => 'A movie with Guy Richie'
+    click_on 'Create Movie'
+    expect(page).to have_content 'Test Title'
+  end
+end
