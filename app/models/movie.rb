56b5684a191eb0163e3daf46fbe1f8539a9c4b89
Jennifer Bell & Lisa Folliard <student@epicodus.com>
2015-10-14 09:58:12 -0700

    validate presence of movie director

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
   validates :title, :presence => true
+  validates :director, :presence => true
+
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Movie do
   it { should validate_presence_of :title }
+  it { should validate_presence_of :director }
+
 end
